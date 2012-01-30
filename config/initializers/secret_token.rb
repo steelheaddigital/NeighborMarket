724d163b7cc52bf745485ab6d2f1666363974145
tom <tmooney3979@gmail.com>
2012-01-30 07:40:10 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GardenMarketplace::Application.config.secret_token = 'eb559bfcee23b2bb6766dcb16e9ba64094ab7ffd8c47a2d9003891c251a00b225bd975c431abb704eea58f3118522c42a0d8e3ffe9b7115e5b53886320c09d7b'
