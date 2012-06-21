80a85d6bfbea65788a95e266d1c2d4f188671a64
tom <tmooney3979@gmail.com>
2012-06-20 19:07:21 -0700

    Fixing issue where quantity was 0 when items added to cart for the first time

--- app/assets/javascripts/cart.js
+++ app/assets/javascripts/cart.js
@@ -4,19 +4,7 @@ $(document).on("submit", ".addToCartButton", function(event){
     var url = $(this).attr("action");
     var data = $(this).serialize();
     
-    $.ajax({
-       type: "POST",
-       url: url,
-       data: data,
-       success: function(data){
-           $("#AddToCartNotice").html("Item added to your cart!").show();
-       },
-       error: function(request){
-
-       }
-    });
-    
-    $.post(url, function() {
+    $.post(url, data, function() {
            $("#AddToCartNotice").html("Item added to your cart!").show();
        }
     );
