d15a2ebba54b3d7d792c22105c23e2d9be3bb077
tom <tmooney3979@gmail.com>
2012-07-06 08:54:38 -0700

    fixing the load script for test inventory items

--- script/load_inventory_items.rb
+++ script/load_inventory_items.rb
@@ -1,5 +1,7 @@
 InventoryItem.transaction do
   (1..100).each do |i|
-    InventoryItem.create(top_level_category_id: 1, second_level_category_id: 1, user_id: 2, name: "test-#{i}", price: 2, price_unit: "each", quantity_available: 5, description: "Lorem Ipsum Doler")
+    item = InventoryItem.new(top_level_category_id: 1, second_level_category_id: 1, name: "test-#{i}", price: 2, price_unit: "each", quantity_available: 5, description: "Lorem Ipsum Doler")
+    item.user_id = 2
+    item.save
   end
 end
