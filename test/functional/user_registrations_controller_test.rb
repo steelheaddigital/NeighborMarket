035d6265b9e6c96d2b5c75d560b031ad5619f371
tom <tmooney3979@gmail.com>
2012-08-02 21:47:37 -0700

    Added some tests for registrations controller. Fixed a bug with the Devise routes

--- test/functional/user_registrations_controller_test.rb
+++ test/functional/user_registrations_controller_test.rb
@@ -1,7 +1,51 @@
 require 'test_helper'
 
 class UserRegistrationsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::TestHelpers
+  
+  def setup
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+  end
+  
+  test "should get new for buyer" do    
+    get :new, :user => {:user_type => "buyer"}
+    
+    assert_response :success
+  end
+  
+  test "should get new for seller" do    
+    get :new, :user => {:user_type => "seller"}
+    
+    assert_response :success
+  end
+  
+  test "should create user" do
+    assert_difference 'User.count' do
+      post :create, :user => { :username => "create", :user_type => "buyer", :email => "create@create.com", :password => "Abc123!", :password_confirmation => "Abc123!", :first_name => "create", :last_name => "create", :initial => "c", :phone => "503-123-4567", :address => "123 Test", :city => "Portland", :country => "US", :state => "OR", :zip => "97218", :delivery_instructions => "bring it" }
+    end
+    
+    assert_redirected_to root_path
+    assert_equal 'Welcome! You have signed up successfully.', flash[:notice]
+  end
+  
+  test "should respond with inactive signup if user type is seller" do
+    assert_difference 'User.count' do
+      post :create, :user => { :username => "createseller", :user_type => "seller", :email => "createseller@create.com", :password => "Abc123!", :password_confirmation => "Abc123!", :first_name => "create", :last_name => "create", :initial => "c", :phone => "503-123-4567", :address => "123 Test", :city => "Portland", :country => "US", :state => "OR", :zip => "97218", :payment_instructions => "bring it" }
+    end
+    
+    assert_redirected_to user_inactive_signup_path
+    assert_equal 'You have signed up successfully. However, we could not sign you in because your account is not_approved.', flash[:notice]
+  end
+  
+  test "should update user" do
+    user  = users(:buyer_user)
+    sign_in user
+    
+    put :update, :user => { :username => "updatebuyer", :email => "createseller@create.com", :current_password => user.password, :first_name => "update", :last_name => "update", :initial => "c", :phone => "503-123-4567", :address => "123 Test", :city => "Portland", :country => "US", :state => "OR", :zip => "97218", :delivery_instructions => "bring it" }
+    
+#    assert_redirected_to root_path
+    assert_equal 'You updated your account successfully.', flash[:notice]
+  end
+  
+  
 end
