beaa76de16f1ff4c72efcc71af2cfe2fdfc03975
tom <tmooney3979@gmail.com>
2012-08-26 07:39:05 -0700

    Editing application.html.erb to load font since it's not loading when assets are precompiled in production Adding imagemagick path to development paperclip configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,4 +37,6 @@ GardenMarketplace::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+  
+  Paperclip.options[:command_path] = "/usr/local/bin/"
 end
