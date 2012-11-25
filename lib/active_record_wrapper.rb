e1d9b6a52209313ce52cfcf9e4f2870cbfd0ed21
tom <tmooney3979@gmail.com>
2012-11-25 08:30:08 -0800

    Getting correct db configuration for RAILS_ENV in ActiveRecordWrapper

--- lib/active_record_wrapper.rb
+++ lib/active_record_wrapper.rb
@@ -8,8 +8,9 @@ module ActiveRecordWrapper
   require_relative '../app/models/site_setting'
   require_relative '../app/models/session'
 
+  env = ENV["RAILS_ENV"] ? ENV["RAILS_ENV"] : 'development'
   dbconfig = YAML::load(File.open('config/database.yml'))
-  ActiveRecord::Base.establish_connection(dbconfig['development'])
+  ActiveRecord::Base.establish_connection(dbconfig[env])
   ActiveRecord::Base.logger = Logger.new(STDERR)
   
 end
