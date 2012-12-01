0993ffcda54186468ea21a9382e4290a41de9e31
tom <tmooney3979@gmail.com>
2012-12-01 07:06:43 -0800

    Fixing session cleaner

--- lib/tasks/active_record_wrapper.rb
+++ lib/tasks/active_record_wrapper.rb
@@ -6,6 +6,8 @@ module ActiveRecordWrapper
 
   require_relative '../../app/models/site_setting'
   require_relative '../../app/models/session'
+  require_relative '../../app/models/cart'
+  require_relative '../../app/models/cart_item'
   
   env = ENV["RAILS_ENV"] ? ENV["RAILS_ENV"] : 'production'
   dbconfig = YAML::load(File.open('config/database.yml'))
