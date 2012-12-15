d0d636570e70f6b536f1cb6d17f023368d4764a1
tom <tmooney3979@gmail.com>
2012-12-15 09:49:31 -0800

    Adding Timoutable and Rememberable to user model Removing session_cleaner.rake as it's not needed with devise Rememberable

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,7 +30,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :timeoutable
 
   # Virtual attribute for authenticating by either username or email
   # This is in addition to a real persisted field like 'username'
--- config/schedule.rb
+++ config/schedule.rb
@@ -19,12 +19,6 @@
 
 # Learn more: http://github.com/javan/whenever
 
-
-set :output, 'log/schedule.log'
-every 5.minutes do
-  rake 'session_cleaner:clean'
-end
-
 set :output, 'log/schedule.log'
 every 1.minute do
   rake 'jobs:execute'
