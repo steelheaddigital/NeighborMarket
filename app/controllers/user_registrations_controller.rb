597b418ed4becf3cdf04f7a9b660b4e51987bafa
tom <tmooney3979@gmail.com>
2013-01-20 16:19:09 -0800

    Fixing issue with wrong message when user becomes a seller

--- app/controllers/user_registrations_controller.rb
+++ app/controllers/user_registrations_controller.rb
@@ -47,6 +47,8 @@ class UserRegistrationsController < Devise::RegistrationsController
     end
     
     self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+    prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+    
     if params[:user][:become_seller] == "true"
       resource.become_seller = true
       add_role(resource, "seller")
@@ -58,19 +60,18 @@ class UserRegistrationsController < Devise::RegistrationsController
 
     if resource.update_attributes(params[resource_name])
       if is_navigational_format?
-        if resource.respond_to?(:pending_reconfirmation?) && resource.pending_reconfirmation?
-          flash_key = :update_needs_confirmation
-        end
+        flash_key = update_needs_confirmation?(resource, prev_unconfirmed_email) ?
+          :update_needs_confirmation : :updated
         # customized code begin
-        if params[:user][:become_seller] == "true"
-          set_flash_message :notice, flash_key || :became_seller
+        if resource.become_seller
+          set_flash_message :notice, :became_seller
           send_new_seller_email(resource)
         else
-          set_flash_message :notice, flash_key || :updated
+          set_flash_message :notice, flash_key
         end
         # customized code end
       end
-      sign_in resource_name, resource, :bypass => true
+      sign_in resource_name, resource, :bypass => true if !resource.become_seller && !resource.become_buyer
       respond_with resource, :location => after_update_path_for(resource)
     else
       clean_up_passwords resource
