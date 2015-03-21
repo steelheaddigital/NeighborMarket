96733a635b8f938cdcb36728312cb3da8e8956ff
tom <tmooney3979@gmail.com>
2015-03-21 12:35:31 -0700

    Switching to cookie session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,9 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
-# Rails.application.config.session_store :cookie_store, key: '_neighbor_market_session'
+Rails.application.config.session_store :cookie_store, key: '_neighbor_market_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
- Rails.application.config.session_store :active_record_store
- ActiveRecord::SessionStore::Session.attr_accessible :data, :session_id
+# Rails.application.config.session_store :active_record_store
+# ActiveRecord::SessionStore::Session.attr_accessible :data, :session_id
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150226051722) do
+ActiveRecord::Schema.define(version: 20150321193005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -181,16 +181,6 @@ ActiveRecord::Schema.define(version: 20150226051722) do
     t.datetime "updated_at",                           null: false
     t.boolean  "active",                default: true
   end
-
-  create_table "sessions", force: true do |t|
-    t.string   "session_id", null: false
-    t.text     "data"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", using: :btree
-  add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at", using: :btree
 
   create_table "site_contents", force: true do |t|
     t.text    "site_description"
